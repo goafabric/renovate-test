df3395a3cb4df8d301238890104ed9fdc58609cb
Andreas Mautsch <amautsch>
2025-08-06 08:25:14 +0200

    init

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,2 +1,3 @@
-val baseImage = "ibm-semeru-runtimes:open-21.0.8_9-jre@sha256:551139c6639d176c9591c2e2eee16b0092b97a31761c8a9202cf9fffc844d845"
+FROM ibm-semeru-runtimes:open-21.0.8_9-jre@sha256:551139c6639d176c9591c2e2eee16b0092b97a31761c8a9202cf9fffc844d845
+//val baseImage = "ibm-semeru-runtimes:open-21.0.8_9-jre@sha256:551139c6639d176c9591c2e2eee16b0092b97a31761c8a9202cf9fffc844d845"
 //val baseImage: String = file("Dockerfile").readText().removePrefix("FROM").trim()
